674386cac92ad17e66a9e02327580da1eb8fddee
Arthur Hertzel <82896326+arthurhertzel@users.noreply.github.com>
2026-05-26 23:10:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const cards = document.querySelectorAll('.card');
 
+cards.forEach(card => {
+  card.addEventListener('mouseenter', () => {
+    card.style.transform = 'scale(1.02)';
+    card.style.transition = '0.3s';
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = 'scale(1)';
+  });
+});
+
+console.log('Site carregado com sucesso!');
